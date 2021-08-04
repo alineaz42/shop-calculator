f4bec7eb56439c9a227d299996e213f799247270
Ali Neaz <neazali77@gmail.com>
2021-08-05 01:46:34 +0600

    put the code in a try catch block

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ sum = 0
 while True:
     userInput = input("Enter the item price or q to quit : \n")
     if userInput != "q":
-        sum = sum+int(userInput)
-        print(f"order total so far {sum}")
+        try:
+            sum = sum+int(userInput)
+            print(f"order total so far {sum}")
+        except ValueError as e:
+            print("Make sure you enter a number")
 
     else:
         print(f"Your total bill is {sum} ")
